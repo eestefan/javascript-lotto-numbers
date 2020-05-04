d35804ed85375a96e04fc85f0f5232a10e192654
eestefan <elizabeth.estefan@gmail.com>
2020-05-04 09:45:24 -0400

    Initial commit

---
+++ js/lotto.js
@@ -0,0 +1,39 @@
+function lottoQuickPick() {
+	var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53];
+  var number;
+  var random=[];
+  for (number=1; number<=6; ++number) {
+    var i = Math.floor((Math.random() * (53-number)) + 1);
+    random.push(array[i]);
+    array[i] = array[53-number];
+    }
+    var sixNumbers = "";
+    for (i = 0; i < 6; i++)
+    {
+      sixNumbers += random[i] + " ";
+    }
+    document.getElementById("lottoQuickPick").innerHTML = sixNumbers;
+}
+
+function powerballQuickPick() {
+	var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69];
+  var number;
+  var random=[];
+  for (number=1; number<=5; ++number) {
+    var i = Math.floor((Math.random() * (69-number)) + 1);
+    random.push(array[i]);
+    array[i] = array[69-number];
+    }
+    var fiveNumbers = "";
+    for (i = 0; i < 5; i++)
+    {
+      fiveNumbers += random[i] + " ";
+    }
+    document.getElementById("powerballQuickPick").innerHTML = fiveNumbers;
+}
+
+function powerballNumber () {
+	var randomNumber = Math.random () * 26;
+	var roundedNumber = Math.ceil(randomNumber);
+	document.getElementById("message").innerHTML = roundedNumber;
+}
